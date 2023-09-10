f54b6c7f09ee21cf21f8d1c91727037700ec0511
ridhigoyall <126961829+ridhigoyall@users.noreply.github.com>
2023-09-10 23:13:51 +0530

    Add files via upload

--- chapter-3.cpp
+++ chapter-3.cpp
@@ -1,21 +1,21 @@
-//pass by reference/pass by value with pointers
-#include<iostream>
+// pass by reference/pass by value with pointers
+#include <iostream>
 using namespace std;
 void passByValue(int val)
 {
-    val=100;
+    val = 100;
 }
 void passByRefrence(int *val)
 {
-    *val=100;
+    *val = 100;
 }
 int main()
 {
-    int x=20;
-    int y=20; 
-    passByValue(x);
-    cout<<x<<endl;
-     passByRefrence(&y);
-    cout<<y<<endl;
+    int x = 20;
+    int y = 20;
+    passByRefrence(&x);
+    cout << x << endl;
+    passByValue(y);
+    cout << y << endl;
     return 0;
 }
